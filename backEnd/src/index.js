2dfa2de757067b58e8202ee1d1492921ccc40a69
ahak14 <ah.abbasi.ah@gmail.com>
2021-01-09 16:45:13 +0330

    added parse server in backend

---
+++ backEnd/src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const ParseServer = require('parse-server').ParseServer;
+const ParseDashboard = require('parse-dashboard');
+const app = express();
+
+const api = new ParseServer({
+  databaseURI: 'mongodb://localhost:27017/dev', // Connection string for your MongoDB database
+//   cloud: './cloud/main.js', // Path to your Cloud Code
+  appId: 'myAppId',
+  masterKey: 'myMasterKey', // Keep this key secret!
+  fileKey: 'optionalFileKey',
+  serverURL: 'http://localhost:1337/parse' // Don't forget to change to https if needed
+});
+
+// Serve the Parse API on the /parse URL prefix
+app.use('/parse', api);
+
+const dashboard = new ParseDashboard({
+  "apps": [
+    {
+      "serverURL": "http://localhost:1337/parse",
+      "appId": "myAppId",
+      "masterKey": "myMasterKey",
+      "appName": "MyApp"
+    }
+  ]
+});
+
+// make the Parse Dashboard available at /dashboard
+app.use('/dashboard', dashboard);
+
+app.listen(1337, function() {
+  console.log('parse-server-example running on port 1337.');
+});
