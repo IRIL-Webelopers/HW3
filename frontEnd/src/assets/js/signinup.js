a2f2bdbe3c5ad1903867764e656e86ab37e3a473
ahak14 <ah.abbasi.ah@gmail.com>
2021-01-28 13:48:46 +0330

    Added validity checker for sign in and sign up forms

--- frontEnd/src/assets/js/signinup.js
+++ frontEnd/src/assets/js/signinup.js
@@ -15,3 +15,79 @@ document.getElementById('signup-id').addEventListener('click', () => {
 document.getElementById('return-home-id').addEventListener('click', () => {
     document.location = "../index.html"
 })
+
+/*
+*   Email input validity checking
+*/
+
+let emailInputs = document.getElementsByName('email-input')
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+for (let i = 0; i < emailInputs.length; i++) {
+    emailInputs[i].addEventListener('keyup', () => {
+        if (emailRegex.test(emailInputs[i].value)) {
+            emailInputs[i].style.borderColor = 'green'
+        } else {
+            emailInputs[i].style.borderColor = 'red'
+        }
+        enableSignupButton()
+        enableSigninButton()
+    })
+}
+
+/*
+*   Password input validity checking
+*/
+
+let passwordInputs = [
+    document.getElementById('signin-password-input-id'),
+    document.getElementById('signup-password-input-id')]
+for (let i = 0; i < passwordInputs.length; i++) {
+    passwordInputs[i].addEventListener('keyup', () => {
+        if (passwordInputs[i].value.length >= 6) {
+            passwordInputs[i].style.borderColor = 'green'
+        } else {
+            passwordInputs[i].style.borderColor = 'red'
+        }
+        enableSignupButton()
+        enableSigninButton()
+    })
+}
+
+let signupPasswordInput = document.getElementById('signup-password-input-id')
+let repeatedPasswordInput = document.getElementById('repeated-password-input-id')
+repeatedPasswordInput.addEventListener('keyup', () => {
+    if (repeatedPasswordInput.value === signupPasswordInput.value) {
+        repeatedPasswordInput.style.borderColor = signupPasswordInput.style.borderColor
+    } else {
+        repeatedPasswordInput.style.borderColor = 'red'
+    }
+    enableSignupButton()
+})
+
+document.getElementById('rules-acceptation-id').addEventListener('click', () => {
+    enableSignupButton()
+})
+
+function enableSignupButton() {
+    let repeatedPasswordInput = document.getElementById('repeated-password-input-id')
+    let signupPasswordInput = document.getElementById('signup-password-input-id')
+    let signupEmailInput = document.getElementById('signup-email-input-id')
+    document.getElementById('signup-button-id').disabled =
+        !(
+            emailRegex.test(signupEmailInput.value) &&
+            repeatedPasswordInput.value === signupPasswordInput.value &&
+            signupPasswordInput.value.length >= 6 &&
+            document.getElementById('rules-acceptation-id').checked
+        )
+}
+
+function enableSigninButton() {
+    let signinEmailInput = document.getElementById('signin-email-input-id')
+    let signinPasswordInput = document.getElementById('signin-password-input-id')
+    document.getElementById('signin-button-id').disabled =
+        !(
+            emailRegex.test(signinEmailInput.value) &&
+            signinPasswordInput.value.length >= 6
+        )
+}
+
